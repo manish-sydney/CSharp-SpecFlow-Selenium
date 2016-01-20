1998b1adc8ea052ca1a9b94ce020832fe550a715
KevinMarkVI <kevin@saucelabs.com>
2016-01-20 13:37:07 -0800

    Add Framework

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# CSharp-SpecFlow-Selenium
+## CSharp-Specflow-Selenium
+
+>This code is presented as an example only, since your tests and testing environments may require specialized scripting. This information should be taken only as an
+>illustration of how one would set up tests with Sauce Labs, and any modifications will not be supported. For questions regarding Sauce Labs integration, please see 
+>our documentation at https://wiki.saucelabs.com/.
+
+### Environment Setup
+
+## Note: Purchasing [SpecFlow+](http://www.specflow.org/plus/) is needed to run tests in parallel.
+
+1. Dependencies:
+    * MS Visual Studio 2013 or later.
+    * Install the SpecFlow extension in Visual Studio
+
+2. Sauce Credentials
+    * Create the following system environmental variables:
+    ```
+    SAUCE_USERNAME=<your Sauce Labs username>
+    SAUCE_ACCESS_KEY=<your Sauce Labs access key>
+    ```
+### Running Tests
+
+Clean and Build the project
+
+Use the VS test runner to "Run All"
+
+[Sauce Labs Dashboard](https://saucelabs.com/beta/dashboard/)
+
+### Resources
+##### Sauce Labs Documentation: 
+* https://wiki.saucelabs.com/
+
+##### SeleniumHQ Documentation:
+* http://www.seleniumhq.org/docs/
+
+##### SpecFlow Documentation:
+* http://www.specflow.org/plus/
+
+##### Stack Overflow:
+* A great resource to search for issues not explicitly covered by documentation.
+* http://stackoverflow.com/
+
+### Known Issues
+* Cross browser testing still in progress
---
+++ SauceLabsSpecflowDemo/Hooks1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SauceLabsSpecflowDemo
+{
+    [Binding]
+    public sealed class Hooks1
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability(CapabilityType.BrowserName, "chrome");
+            capabilities.SetCapability(CapabilityType.Version, "47.0");
+            capabilities.SetCapability(CapabilityType.Platform, "OS X 10.10");
+            capabilities.SetCapability("username", Environment.GetEnvironmentVariable("SAUCE_USERNAME"));
+            capabilities.SetCapability("accessKey", Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY"));
+            capabilities.SetCapability("name", ScenarioContext.Current.ScenarioInfo.Title);
+
+            RemoteWebDriver webDriver = new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), capabilities, TimeSpan.FromSeconds(600));
+            webDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(20));
+            ScenarioContext.Current["driver"] = webDriver;
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            IWebDriver driver = (IWebDriver)ScenarioContext.Current["driver"];
+            bool passed = ScenarioContext.Current.TestError == null;
+            ((IJavaScriptExecutor)driver).ExecuteScript("sauce:job-result=" + (passed ? "passed" : "failed"));
+            driver.Quit();
+        }
+    }
+}
+
